35eb762668b902c9e5344759acbfb6de170e23c5
lsu <lsu@adobe.com>
2025-05-02 21:42:45 -0700

    adding an alert

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -15,6 +15,7 @@ export default async function decorate(block) {
   block.textContent = '';
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
-
+ 
   block.append(footer);
+  alert('longsu in footer.js');
 }
